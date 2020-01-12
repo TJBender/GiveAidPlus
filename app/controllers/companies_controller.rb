1c8545a2d4e85c500f38a9c3443cf2a8fecc1daa
TJBender <tiana.bender01@gmail.com>
2020-01-11 21:34:59 -0500

    worked on controllers. basic JSON format. seeds working

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,3 +1,24 @@
 class CompaniesController < ApplicationController
+    def index
+        render json: Company.all
+    end
 
+    def show
+        @company = Company.find(params[:id])
+        render json: {company: @company, jobs: @company.jobs, volunteers: @company.volunteers}
+        
+    end
+
+    def create
+        @company = Company.create(company)
+        render json: @company
+    end
+
+    private
+
+    def company_params
+        params.require(:company).permit(:name)
+        # work on this later and implement password of some sort
+        # even if its just a find_or_create_by
+    end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,13 @@ class Job < ApplicationRecord
     belongs_to :company
     has_many :volunteer_jobs
     has_many :volunteers, through: :volunteer_jobs
+
+    after_create :add_user_as_volunteer 
+
+    private 
+    # create the connection?
+    def add_user_as_volunteer 
+        VolunteerJob.create(company: self.company, job: self)
+    end
+
 end
