852c7baafc11ec4d25e25be1b6e1d4341b3c2975
TJBender <tiana.bender01@gmail.com>
2020-01-15 09:04:53 -0500

    login and volunteer can see all available jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,19 @@
 # Volunteers
-v1 = Volunteer.create(name: "Eva", password: "123", phone_number: "2012222222")
-v2 = Volunteer.create(name: "Jim", password: "123", phone_number: "2012223333")
-v3 = Volunteer.create(name: "Sue", password: "123", phone_number: "2014442222")
-v4 = Volunteer.create(name: "Bob", password: "123", phone_number: "2015552222")
+v1 = Volunteer.create(name: "Danny", password: "123", phone_number: "2012222222")
+v2 = Volunteer.create(name: "Tiana", password: "123", phone_number: "2012223333")
+v3 = Volunteer.create(name: "Babs", password: "123", phone_number: "2014442222")
+v4 = Volunteer.create(name: "Jack", password: "123", phone_number: "2015552222")
 
 # Companies
-c1 = Company.create(name: "Hout Sauce")
-c2 = Company.create(name: "Steaks and Things")
-c3 = Company.create(name: "Meatballs and Burgers")
+c1 = Company.create(name: "The American Red Cross")
+c2 = Company.create(name: "Charity Water")
+c3 = Company.create(name: "Make A Wish")
 
 # Jobs
-j1 = c1.jobs.create(name: "Eat steak", hours: 8)
-j2 = c2.jobs.create(name: "Eat beef", hours: 9)
-j3 = c3.jobs.create(name: "Eat chicken?", hours: 1)
-j4 = c3.jobs.create(name: "Hard ass work", hours: 12)
+j1 = c1.jobs.create(name: "Receptionist", hours: 4)
+j2 = c2.jobs.create(name: "Guest Greeter", hours: 2)
+j3 = c3.jobs.create(name: "Bartender", hours: 1)
+j4 = c3.jobs.create(name: "Janitor", hours: 12)
 
 # Volunteer Jobs (-this is the join model between jobs and volunteers-)
 v1.jobs <<  j1
