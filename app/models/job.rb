6b9d0262f4cc1acb88b316ab1485a3b4899cc894
TJBender <tiana.bender01@gmail.com>
2020-01-16 10:18:08 -0500

    added new job seeds

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,12 +2,4 @@ class Job < ApplicationRecord
     belongs_to :company
     has_many :volunteer_jobs
     has_many :volunteers, through: :volunteer_jobs
-
-    # after_create :add_user_as_volunteer 
-
-    # private 
-    # # create the connection?
-    # def add_user_as_volunteer 
-    #     VolunteerJob.create(company: self.company, job: self)
-    # end
 end
