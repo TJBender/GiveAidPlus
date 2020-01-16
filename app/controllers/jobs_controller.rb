00beb369c84c44bc25181386230d879a090433c4
TJBender <tiana.bender01@gmail.com>
2020-01-16 11:46:46 -0500

    cleaning up messy comments

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,7 +6,6 @@ class JobsController < ApplicationController
 
     def show
         @job = Job.find(params[:id])
-        # render what you want to be able to see on the front end from here
         render json: {job: @job, company: @job.company, volunteers: @job.volunteers}
     end
 
@@ -14,12 +13,6 @@ class JobsController < ApplicationController
         @job = Job.create(job_params)
         render json: @job
     end
-
-    # def user_job
-    #     @job = Job.find(params[:id])
-    #     @volunteers = @job.volunteers
-    #     render json: {volunteers: @volunteers, total_hours: @volunteer_jobs.sum(:hours)}
-    # end
 
     private
 
